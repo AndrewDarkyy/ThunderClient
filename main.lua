1dc88d3ee8500460c34a8f98073ef36feff1086c
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2023-09-28 09:40:26 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
 -- thunder client & darkyyware
-repeat wait() until game:IsLoaded()
+
+repeat task.wait() until game:IsLoaded()
+task.wait(0.5) -- luarmor may crash if you execute tooo early
+
 if game.PlaceId == 286090429 or game.PlaceId == 11924224825 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dca3e69649ed196af0ac6577f743a0ae.lua"))() -- thunder client
 elseif game.PlaceId == 621129760 or game.PlaceId == 4249058633 or game.PlaceId == 3527629287 or game.PlaceId == 4505939773 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53db65a6472cddecdbdc4f5445993466.lua"))() -- darkyyware
 else
-    game.Players.LocalPlayer.kick(game.Players.LocalPlayer, "This game is NOT supported")
+    game.Players.LocalPlayer:Kick("This game is NOT supported")
 end
