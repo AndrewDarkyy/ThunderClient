a8b5baea5bb428958426e1268285dcbee6b23d00
AndrewDarkyy <129768810+AndrewDarkyy@users.noreply.github.com>
2023-09-28 09:41:37 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,5 +8,5 @@ if game.PlaceId == 286090429 or game.PlaceId == 11924224825 then
 elseif game.PlaceId == 621129760 or game.PlaceId == 4249058633 or game.PlaceId == 3527629287 or game.PlaceId == 4505939773 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53db65a6472cddecdbdc4f5445993466.lua"))() -- darkyyware
 else
-    game.Players.LocalPlayer:Kick("This game is NOT supported")
+    game.Players.LocalPlayer:Kick("this script only works in arsenal/kat/big paintball")
 end
